6af3d09199253872ba092ad8428632719224fe04
anqi-h <60205281+anqi-h@users.noreply.github.com>
2020-01-31 16:54:02 +0800

    Add files via upload

--- week 3_nysha and anqi/sketch.js
+++ week 3_nysha and anqi/sketch.js
@@ -1,66 +1,22 @@
 let bulbOn;
 let bulbOff;
 
-let brightness = 0;
+let brightness = 0; 
 
 function setup() {
   createCanvas(400, 400);
-  bulbOn =loadImage("images/bulb_on.png");
-  bulbOff =loadImage("images/bulb_off.png");
+  bulbOn = loadImage (Images/bulb_on.png");
+  bulbOff = loadImage (Images/bulb_off.png");
+
 }
 
 function draw() {
   background(220);
-  //image(bulbOn, 0, 0);
-  
   imageMode(CENTER);
   
-  if(brightness == 1)
-  { 
-  
-    image(bulbOn, width/2, height/2, 300, 300)
-    brightness = !brightness; //challenge on off
-                     }
-  else { 
-  image(bulbOff, width/2, height/2, 300, 300);
-   
+  if(brightness ==1){
+image(bulbOn,0,0, 300,300);
+}else{
+  image(bulbOff,0,0, 300,300)
   }
-  
-} 
-
-function on (){
-brightness=1;
-print("the bulb is on~!!!!");  
 }
-
-function off (){
-brightness=0;
-  print("the bulb is off!!!");
-}
-
-function mousePressed(){
-  on();
-}
-
-function mouseReleased(){
-  off()
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
